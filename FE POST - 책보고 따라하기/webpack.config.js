68b99c97ea8cb80f81a8c2c67e883f32770cc2bc
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2023-02-13 20:27:16 +0900

    Update webpack.config.js

--- FE POST - 책보고 따라하기/webpack.config.js
+++ FE POST - 책보고 따라하기/webpack.config.js
@@ -1,52 +1,7 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
+const developmentConfig = require('./webpack.dev.config');
+const productionConfig = require('./webpack.prod.config');
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
-module.exports = {
-  entry: "./src/js/main.js",
-  output: {
-    path: path.resolve(process.cwd(), "dist"),
-    filename: '[name].[contenthash].js',
-    environment: {
-        arrowFunction: false,
-    },
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: ["file-loader"],
-      },
-    ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-      new CssMinimizerPlugin(),
-    ],
-  },
+module.exports = (env, {mode}) => {
+  return mode === 'production' ? productionConfig : developmentConfig;
 };
