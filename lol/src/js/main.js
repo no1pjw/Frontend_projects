a64a25f673ffc683cf9f4cd281774eac8cbad26d
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2023-02-15 20:06:42 +0900

    Update main.js

--- lol/src/js/main.js
+++ lol/src/js/main.js
@@ -1,4 +1,5 @@
 const button = document.getElementById('user_search');
+const api_key = "**********************";
 button.addEventListener('click', (ev) => {
     var input = document.getElementById('user_name').value;
     console.log(input)
@@ -6,12 +7,13 @@ button.addEventListener('click', (ev) => {
         method: 'GET',
         redirect: 'follow'
       };
-    fetch("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + input + "?api_key=RGAPI-a08f0973-3650-4e0d-a640-699c804b1e80", requestOptions)
+    fetch("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + input + "?api_key="+api_key, requestOptions)
                 .then(response=> response.json())
                 .then(result => result.id)
-                .then(name => {fetch('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + name +'?api_key=RGAPI-a08f0973-3650-4e0d-a640-699c804b1e80', requestOptions)
+                .then(name => {fetch('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + name +'?api_key='+api_key, requestOptions)
                 .then(response => response.text())
                 .then(result => console.log(result))
+                .catch(error=> console.log('error', error));
                 })
         .catch(error => console.log('error', error));
 })
