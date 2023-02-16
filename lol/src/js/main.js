4f8ea15d45667d06e29f0cd0f92fe868220f5b24
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2023-02-16 19:35:42 +0900

    Update main.js

--- lol/src/js/main.js
+++ lol/src/js/main.js
@@ -1,5 +1,6 @@
 const button = document.getElementById('user_search');
-const api_key = "***********************";
+const api_key = "RGAPI-e0e9241c-6d75-4cd4-b2e5-d0a0e900a0cd";
+const Info_list = new Array(3);
 var requestOptions = {
   method: 'GET',
   redirect: 'follow'
@@ -13,6 +14,17 @@ async function get_userInfo(input) {
   const post= await postResponse.json();
   const post_id = await post['id'];
   const postResponse_2 = await fetch('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + post_id +'?api_key='+api_key, requestOptions);
-  const post_2 = await postResponse_2.json();
-  console.log(post_2[0]['wins']);
+  const userInfo = await postResponse_2.json();
+  for(var i = 0; i < 3; i++){
+    console.log(userInfo[i]);
+    try{
+      const win  = userInfo[i]['wins'];
+      Info_list[i] = userInfo[i];
+    }
+    catch{
+      Info_list[i] = 'unranked';
+    }
+    
+  }
+  console.log(Info_list);
 }
