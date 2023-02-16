04cfec20ebce991ef6111ae11c3c7e0f70ef8b5c
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2023-02-16 17:45:51 +0900

    Update main.js

--- lol/src/js/main.js
+++ lol/src/js/main.js
@@ -1,19 +1,18 @@
 const button = document.getElementById('user_search');
-const api_key = "**********************";
+const api_key = "***********************";
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
 button.addEventListener('click', (ev) => {
     var input = document.getElementById('user_name').value;
-    console.log(input)
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-    fetch("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + input + "?api_key="+api_key, requestOptions)
-                .then(response=> response.json())
-                .then(result => result.id)
-                .then(name => {fetch('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + name +'?api_key='+api_key, requestOptions)
-                .then(response => response.text())
-                .then(result => console.log(result))
-                .catch(error=> console.log('error', error));
-                })
-        .catch(error => console.log('error', error));
+    get_userInfo(input);
 })
+async function get_userInfo(input) {
+  const postResponse = await fetch("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + input + "?api_key="+api_key, requestOptions);
+  const post= await postResponse.json();
+  const post_id = await post['id'];
+  const postResponse_2 = await fetch('https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/' + post_id +'?api_key='+api_key, requestOptions);
+  const post_2 = await postResponse_2.json();
+  console.log(post_2[0]['wins']);
+}
